7ff6874a762599f31ce38d4343341d72556b17b7
esgariot88 <40865390+esgariot88@users.noreply.github.com>
2022-09-18 13:25:40 +0300

    Update srez.cs
    Вводим второй пустой массив равный длине первого массива и счетчик.

--- srez.cs
+++ srez.cs
@@ -2,3 +2,5 @@ string someString = " Hello, 2, world, :-) ";
 string[] firstStringArray = new string[]{};
 firstStringArray = someString.Split(',');
 firstStringArray = someString.Split(' ');
+string[] secondStringArray =  new string[firstStringArray.Length] ;
+int count = 0;
